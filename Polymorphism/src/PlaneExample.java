81ae753814ac70fce9af6d98e071215b73a9007b
HOME <HOME@FAMILY>
2025-02-05 11:55:23 +0530

    p

---
+++ Polymorphism/src/PlaneExample.java
@@ -0,0 +1,78 @@
+/*
+ * Polymorphism means [1:Many] it means on variable can use for multiple objects
+ * and that one variable is parent type variable of that object.
+ */
+//Example->
+class Plane
+{
+	public void takeoff()
+	{
+		System.out.println("Plane is takking off.");
+	}
+	public void fly()
+	{
+		System.out.println("Plane is flying");
+	}
+	public void landing()
+	{
+		System.out.println("Plane is landing");
+	}
+}
+
+
+class CargoPlane extends Plane
+{
+	public void fly()
+	{
+		System.out.println("Cargo plane is Flying");
+	}
+	public void carryGoods()
+	{
+		System.out.println("Cargo planes carrier goods.");
+	}
+}
+
+
+class PassengerPlane extends Plane
+{
+	public void fly()
+	{
+		System.out.println("Passenger plane is flying.");
+	}
+	public void carryPassenger()
+	{
+		System.out.println("Passenger Plane carries passenger.");
+	}
+}
+class FighterPlane extends Plane
+{
+	public void fly()
+	{
+		System.out.println("Fighter plane flies at high altitude.");
+	}
+}
+
+class Airport
+{
+	public void permit(Plane plane)//Here we store all child object reference in parent type reference variable
+	{								// and here [1:Many] works coz plane is used for all child reference otherwise we have to create different different
+		plane.takeoff();			//method for every child type variable to hold the reference
+		plane.fly();
+		plane.landing();
+	}
+}
+
+public class PlaneExample {
+
+	public static void main(String[] args) {
+		CargoPlane cp=new CargoPlane();
+		PassengerPlane pp=new PassengerPlane();
+		FighterPlane fp=new FighterPlane();
+		Airport a=new Airport();
+		
+		a.permit(cp);
+		a.permit(pp);
+		a.permit(fp);
+	}
+
+}
